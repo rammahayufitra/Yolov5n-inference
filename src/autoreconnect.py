78c76c2da3d34431642f2529afb8c2c127bdb19f
rammafitra <rammahayufitra@gmail.com>
2022-10-13 00:19:59 +0700

    [okt-13-2022] checkpoint6 autoreconnect.py

--- src/autoreconnect.py
+++ src/autoreconnect.py
@@ -1,6 +1,7 @@
 import cv2
 import requests  # NOTE: Only used for forceful reconnection
 import time  # NOTE: Only used for throttling down printing when connection is lost
+from component.helper import *
 
 
 class IPVideoCapture:
@@ -61,14 +62,36 @@ class IPVideoCapture:
 if __name__ == "__main__":
     CAM_ADDRESS = "https://10.162.1.15:8080/video"  # NOTE: Change
     CAM_FORCE_ADDRESS = "http://10.162.1.15:8080/override"  # NOTE: Change or omit
-    cap = IPVideoCapture(CAM_ADDRESS, CAM_FORCE_ADDRESS, blocking=True)
+    cam = IPVideoCapture(CAM_ADDRESS, CAM_FORCE_ADDRESS, blocking=True)
     # cap = IPVideoCapture(CAM_ADDRESS)  # Minimal init example
 
     while True:
-        ret, frame = cap.read()
+        check, image = cam.read()
+        if image is not None:
+            prediction = get_pred(image)
 
-        if ret is True:
-            cv2.imshow(CAM_ADDRESS, frame)
+            if prediction is not None:
+                for pred in prediction:
+                    x1 = int(pred[0])
+                    y1 = int(pred[1])
+                    x2 = int(pred[2])
+                    y2 = int(pred[3])
 
-        if cv2.waitKey(1) == ord("q"):
-            break
+                    start = (x1,y1)
+                    end = (x2,y2)
+
+                    pred_data = f'{label[pred[-1]]} {str(pred[-2]*100)[:5]}%'
+                    print(pred_data)
+                    color = (0,255,0)
+                    image = cv2.rectangle(image, start, end, color)
+                    image = cv2.putText(image, pred_data, (x1,y1+25), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2, cv2.LINE_AA) 
+
+
+            if check is True:
+                cv2.imshow(CAM_ADDRESS, image)
+
+            if cv2.waitKey(1) == ord("q"):
+                break
+
+cam.release()
+cv2.destroyAllWindows()
